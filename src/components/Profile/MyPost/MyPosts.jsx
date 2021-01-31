acafa7d5e025b8d0efe8677d6fc7fa3547400f46
EGalinevsky <eugene.galinevsky@mail.ru>
2021-01-31 10:46:56 +0300

    props

--- src/components/Profile/MyPost/MyPosts.jsx
+++ src/components/Profile/MyPost/MyPosts.jsx
@@ -4,16 +4,14 @@ import Post from "./post/Post";
 const MyPosts = () => {
   return (
     <div className={s.content}>
-      <div>
-        My Posts
-      </div>
+      <div>My Posts</div>
       <div>
         <textarea></textarea>
         <button>send</button>
       </div>
-      <Post />
-      <Post />
-      <Post />
+     <Post message='1'/>
+     <Post message='2'/>
+     <Post message='3'/>
     </div>
   );
 };
