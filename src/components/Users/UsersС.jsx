41c0608d8cb6e96c734255454921e6e48b562bee
eGalinevsky <eugene.galinevsky@mail.ru>
2021-02-25 23:09:39 +0300

    space

--- src/components/Users/UsersС.jsx
+++ src/components/Users/UsersС.jsx
@@ -5,6 +5,7 @@ import user_png from "../../assets/img/user.jpg";
 
 class Users extends React.Component {
     
+    
     componentDidMount(){
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {this.props.setUsers(response.data.items);
             });  
